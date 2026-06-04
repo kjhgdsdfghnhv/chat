1965c93843c966f1bad14e7bb19063c2c5183495
rrr <megarh721@gmail.com>
2026-06-04 12:49:58 +0300

    fix: додано PATCH /api/users/me для зміни імені к

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from '../auth/auth.module';
 import { ChatsModule } from '../chats/chats.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [AuthModule, ChatsModule, UsersModule],
+  imports: [
+    AuthModule,
+    ChatsModule,
+    forwardRef(() => UsersModule),
+  ],
   providers: [ChatGateway],
+  exports: [ChatGateway], // важливо: експортуємо, щоб UsersModule міг його використати
 })
 export class GatewayModule {}
